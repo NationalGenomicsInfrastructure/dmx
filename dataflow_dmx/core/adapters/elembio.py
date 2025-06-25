e352064503fe54b19ba6afe204edc482ee172548
glrs <5999366+glrs@users.noreply.github.com>
2025-06-25 10:39:10 +0200

    refactor: remove run_id parameter from extract_flowcell_id method in AvitiAdapter and PromethIONAdapter

--- dataflow_dmx/core/adapters/elembio.py
+++ dataflow_dmx/core/adapters/elembio.py
@@ -17,22 +17,22 @@ class AvitiAdapter(ElementAdapterMixin):
         # TODO: Check if this is correct for Aviti
         return run_id.startswith("AV")
 
-    def extract_flowcell_id(self, run_id: str) -> str:
+    def extract_flowcell_id(self) -> str:
         # TODO: Check if this is correct for Aviti
-        return run_id.split("_")[2]
+        return self.run_id.split("_")[2]
 
     def build_demux_config(self) -> DemuxConfig:
         cmd = [
-            AVITI_DEMUX,
+            str(AVITI_DEMUX),
             "--input",
-            self.run_path,
+            str(self.run_path),
             "--output",
             "/ngi/fastq",
         ]  # Example command, adjust as needed
 
         return DemuxConfig(
             run_id=self.run_id,
-            flowcell_id=self.extract_flowcell_id(self.run_id),
+            flowcell_id=self.extract_flowcell_id(),
             lanes=[],  # Does Aviti have lanes? If so, how to extract them?
             samplesheet_path=None,  # Does Aviti operate with samplesheets? If so, how to retrieve them?
             cmd=cmd,
--- dataflow_dmx/core/adapters/ont.py
+++ dataflow_dmx/core/adapters/ont.py
@@ -10,16 +10,16 @@ class PromethIONAdapter(InstrumentAdapter):
         # TODO: Check if this is correct for PromethION
         return run_id.startswith("P") or "promethion" in run_id.lower()
 
-    def extract_flowcell_id(self, run_id: str) -> str:
+    def extract_flowcell_id(self) -> str:
         # TODO: Check if this is correct for PromethION
-        return run_id.split("_")[-1]
+        return self.run_id.split("_")[-1]
 
     def build_demux_config(self) -> DemuxConfig:
         # NOTE: ONT PromethION does not require demultiplexing
         # Nothing to run; just inform downstream tasks
         return DemuxConfig(
             run_id=self.run_id,
-            flowcell_id=self.extract_flowcell_id(self.run_id),
+            flowcell_id=self.extract_flowcell_id(),
             lanes=[1],  # lanes irrelevant for PromethION?
             samplesheet_path=None,  # No samplesheets for PromethION?
             cmd=None,  # No command to run for PromethION (no demux needed)
